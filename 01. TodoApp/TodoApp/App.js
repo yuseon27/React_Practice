b6c10d10e7b58fcd77840238be50881794f4e2a1
yuseon27 <ysnam19@naver.com>
2020-04-16 16:04:00 +0900

    Start of Todo App - App title and subtitle

---
+++ 01. TodoApp/TodoApp/App.js
@@ -0,0 +1,40 @@
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+import Header from './app/components/Header';
+import Subtitle from './app/components/Subtitle';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <View style={styles.header_container}>
+        <Header title='TODO'/>
+      </View>
+
+      <View style={styles.subtitle_container}>
+        <Subtitle title='Input your Todo : '/>
+      </View>
+
+      <View style={styles.subtitle_container}>
+        <Subtitle title='Your Todo List : '/>
+      </View>
+
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+  },
+  header_container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  subtitle_container: {
+    marginLeft:30,
+    marginBottom:10,
+  },
+
+
+});
