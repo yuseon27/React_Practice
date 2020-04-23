a13a52edb799714a617f8e1a6ea195f37f13d3af
yuseon27 <ysnam19@naver.com>
2020-04-24 02:32:57 +0900

    Change bg color and icon according to the input

--- 02. WeatherApp/WeatherAppLecture/app/Weather.js
+++ 02. WeatherApp/WeatherAppLecture/app/Weather.js
@@ -1,17 +1,47 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { LinearGradient } from 'expo-linear-gradient';
-import {MaterialCommunityIcons} from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 export default function Weather({temp, title}) {
+    const weather_info = {
+        Thunderstorm: {
+            icon: 'weather-lightning-rainy',
+            backcolor: ['#e9d362', '#333333'],
+        },
+        Drizzle: {
+            icon: 'weather-hail',
+            backcolor: ['#005AA7', '#FFFDE4'],
+        },
+        Rain: {
+            icon: 'weather-pouring',
+            backcolor: ['#3a7bd5', '#3a6073'],
+        },
+        Snow: {
+            icon: 'weather- snowy',
+            backcolor: ['#FFEFBA', '#FFFFFF'],
+        },
+        Mist: {
+            icon: 'weather-fog',
+            backcolor: ['#1c92d2', '#f2fcfe'],
+        },
+        Clear: {
+            icon: 'weather-sunny',
+            backcolor: ['#CAC531', '#F3F9A7'],
+        },
+        Clouds: {
+            icon: 'weather-cloudy',
+            backcolor: ['#bdc3c7', '#2c3e50'],
+        },
+    }
 
     return (
         <LinearGradient
-            colors={['#4c669f', '#3b5998', '#ffffff']}
+            colors={weather_info[title].backcolor}
             style={styles.weather_back}>
 
             <View style={styles.icon_area}>
-                <MaterialCommunityIcons style={styles.icon_custom} name={'weather-sunny'} size={170}/>
+                <MaterialCommunityIcons style={styles.icon_custom} name={weather_info[title].icon} size={170}/>
             </View>
 
             <View style={styles.weather_area}>
