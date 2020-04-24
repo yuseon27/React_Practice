02cc499624e911f081bacf868d83db36b6b612de
yuseon27 <ysnam19@naver.com>
2020-04-25 02:50:30 +0900

    Get current location

--- 02. WeatherApp/WeatherAppLecture/App.js
+++ 02. WeatherApp/WeatherAppLecture/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
 import { StyleSheet, Text, View, ActivityIndicator } from 'react-native';
+import * as Location from 'expo-location';
 
 import Weather from './app/Weather';
 
-// https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f3d674bdcb10c90903255ee16eeeb87d
+// https://api.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=f3d674bdcb10c90903255ee16eeeb87d
 const API_KEY = 'f3d674bdcb10c90903255ee16eeeb87d'    // use "fetch / axios" to use api
 
 
@@ -20,8 +21,13 @@ export default class App extends React.Component {
     this._getWeather()
   }
 
-  _getWeather = async() => {  // async : process sequentially in this function
-    const _response = await fetch(`https://samples.openweathermap.org/data/2.5/weather?lat=35&lon=139&appid=${API_KEY}`)  // ` : backtick , wait until execute ... celsius : &units=metric
+  _getWeather = async() => {  // async : process sequentially in this function using await
+    // await Location.requestPermissionsAsync()  // For allowing permission
+    const _location = await Location.getCurrentPositionAsync()
+    console.log(_location)
+    const {coords:{latitude, longitude}} = _location
+
+    const _response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${latitude}&lon=${longitude}&appid=${API_KEY}&units=metric`)  // ` : backtick , wait until execute ... celsius : &units=metric
     const _json_weather = await _response.json()
 
     this.setState({isloaded : true, temp:Math.floor(_json_weather.main.temp), title:_json_weather.weather[0].main})
