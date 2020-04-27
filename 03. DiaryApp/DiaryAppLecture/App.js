4b003a5c3d35073e9884f51dd18c5df58874eb40
yuseon27 <ysnam19@naver.com>
2020-04-28 06:30:17 +0900

    Finished Navigation

--- 03. DiaryApp/DiaryAppLecture/App.js
+++ 03. DiaryApp/DiaryAppLecture/App.js
@@ -1,10 +1,25 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { NavigationContainer } from '@react-navigation/native';
+
+import MainScreen from './screens/MainScreen';
+import WriteScreen from './screens/WriteScreen';
+import DetailScreen from './screens/DetailScreen';
+
+const BaseNavi = createBottomTabNavigator()
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+      <NavigationContainer>
+        <BaseNavi.Navigator>
+          <BaseNavi.Screen name="Main" component={MainScreen} />
+          <BaseNavi.Screen name="Write" component={WriteScreen} />
+          <BaseNavi.Screen name="Detail" component={DetailScreen} />
+        </BaseNavi.Navigator>
+      </NavigationContainer>
     </View>
   );
 }
@@ -13,7 +28,5 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
   },
 });
