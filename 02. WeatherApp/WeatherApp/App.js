4d170f7a58042c03c94309421cfd2d474303019c
yuseon27 <ysnam19@naver.com>
2020-04-27 13:06:20 +0900

    Loading

--- 02. WeatherApp/WeatherApp/App.js
+++ 02. WeatherApp/WeatherApp/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, ActivityIndicator} from 'react-native';
 import Weather from './app/Weather'
 
 /*
@@ -16,7 +16,9 @@ const API_KEY = 'f3d674bdcb10c90903255ee16eeeb87d'
 export default class App extends React.Component {
   constructor (props) {
     super(props);
-    this.state={}
+    this.state={
+      is_loaded : false,
+    }
   }
 
   componentDidMount() {
@@ -29,13 +31,13 @@ export default class App extends React.Component {
 
     console.log(_json_weather)
 
-    this.setState({temperature:Math.floor(_json_weather.main.temp), icon_msg:_json_weather.weather[0].main})
+    this.setState({is_loaded:true, temperature:Math.floor(_json_weather.main.temp), icon_msg:_json_weather.weather[0].main})
   }
 
   render() {
     return (
       <View style={styles.container}>
-        <Weather temperature={this.state.temperature} icon_msg={this.state.icon_msg}/>
+        {this.state.is_loaded ? <Weather temperature={this.state.temperature} icon_msg={this.state.icon_msg}/> : <ActivityIndicator style={styles.container} color='black' size='large'/>}
       </View>
     );
   }
