c0e259054d4976c2fdc1650b82a2a3d3a7d9737c
yuseon27 <ysnam19@naver.com>
2020-04-29 03:40:48 +0900

    Added Icon in bottom navigation bar

--- 03. DiaryApp/DiaryAppLecture/App.js
+++ 03. DiaryApp/DiaryAppLecture/App.js
@@ -3,6 +3,7 @@ import { StyleSheet, Text, View } from 'react-native';
 import { createStackNavigator, createAppContainer } from 'react-navigation';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import { NavigationContainer } from '@react-navigation/native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 import MainScreen from './screens/MainScreen';
 import WriteScreen from './screens/WriteScreen';
@@ -10,14 +11,46 @@ import DetailScreen from './screens/DetailScreen';
 
 const BaseNavi = createBottomTabNavigator()
 
+const NaviOptions = {
+  Main_Screen : {
+    component : MainScreen,
+    options : {
+      tabBarIcon : ({tintColor}) => (
+        <MaterialCommunityIcons name = "calendar-multiselect" size={30} style={{color:tintColor}}/>
+      )
+    }
+  },
+  Write_Screen : {
+    component : WriteScreen,
+    options : {
+      tabBarIcon : ({tintColor}) => (
+        <MaterialCommunityIcons name = "calendar-multiselect" size={30} style={{color:tintColor}}/>
+      )
+    }
+  },
+  Detail_Screen : {
+    component : DetailScreen,
+    options : {
+      tabBarIcon : ({tintColor}) => (
+        <MaterialCommunityIcons name = "calendar-multiselect" size={30} style={{color:tintColor}}/>
+      )
+    }
+  },
+}
+
+/*
+To hide label in bottom tab bar, 
+in BaseNavi.Navigator,  write tabBarOptions={{showLabel:false}}
+*/
+
 export default function App() {
   return (
     <View style={styles.container}>
       <NavigationContainer>
         <BaseNavi.Navigator>
-          <BaseNavi.Screen name="Main" component={MainScreen} />
-          <BaseNavi.Screen name="Write" component={WriteScreen} />
-          <BaseNavi.Screen name="Detail" component={DetailScreen} />
+          <BaseNavi.Screen name="Write" component={NaviOptions.Write_Screen.component} options={NaviOptions.Write_Screen.options} />
+          <BaseNavi.Screen name="Main" component={NaviOptions.Main_Screen.component} options={NaviOptions.Main_Screen.options}/>
+          <BaseNavi.Screen name="Detail" component={NaviOptions.Detail_Screen.component} options={NaviOptions.Detail_Screen.options} />
         </BaseNavi.Navigator>
       </NavigationContainer>
     </View>
--- 03. DiaryApp/DiaryAppLecture/screens/MainScreen.js
+++ 03. DiaryApp/DiaryAppLecture/screens/MainScreen.js
@@ -1,13 +1,23 @@
 import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import { SafeAreaView } from 'react-navigation';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
-export default function MainScreen() {
-  return (
-    <SafeAreaView style={styles.container}>
-      <Text style={styles.text_style}>Main Screen</Text>
-    </SafeAreaView>
-  );
+export default class MainScreen extends React.Component {
+  // Not working this way but don't know why...
+  // static options = {
+  //   tabBarIcon : ({tintColor}) => (
+  //     <MaterialCommunityIcons name = "calendar-multiselect" size={30} style={{color:tintColor}}/>
+  //   )
+  // }
+
+  render() {
+    return (
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.text_style}>Main Screen</Text>
+      </SafeAreaView>
+    );
+  }
 }
 
 const styles = StyleSheet.create({
